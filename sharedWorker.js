f412cd46d4c3c683cee6bfd79c749323ffa3c924
wuqiongjie <wuqiongjie@users.noreply.github.com>
2019-09-22 00:16:55 +0800

    添加 sharedWorker 文件

---
+++ sharedWorker.js
@@ -0,0 +1,13 @@
+var clients = [];
+onconnect = function(e) {
+  var port = e.ports[0];
+  clients.push(port);
+  port.addEventListener('message', function(e) {
+    for(var i = 0; i < clients.length; i++) {
+      var eElement = clients[i];
+      eElement.postMessage(e.data);
+    }
+  });
+  
+  port.start();
+}
