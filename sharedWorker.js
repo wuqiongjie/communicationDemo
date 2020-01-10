e8a83d0bc3bac474296a8317d5299b7d15d156b0
qjie wu <wuqiongjie@users.noreply.github.com>
2020-01-10 10:57:21 +0800

    Update sharedWorker.js

--- sharedWorker.js
+++ sharedWorker.js
@@ -3,8 +3,9 @@ onconnect = function(e) {
   var port = e.ports[0];
   clients.push(port);
   port.addEventListener('message', function(e) {
-    for(var i = 0; i < clients.length; i++) {
-      var eElement = clients[i];
+    let tempClients = clients.filter(v => v!= port )
+    for(var i = 0; i < tempClients.length; i++) {
+      var eElement = tempClients[i];
       eElement.postMessage(e.data);
     }
   });
