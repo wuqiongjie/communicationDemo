aaccd9264fd39571ad9d1c000eb06ae75b3dfb5f
qjie wu <wuqiongjie@users.noreply.github.com>
2020-01-10 14:29:39 +0800

    Update sharedWorker.js

--- sharedWorker.js
+++ sharedWorker.js
@@ -3,7 +3,6 @@ onconnect = function(e) {
   var port = e.ports[0];
   clients.push(port);
   port.addEventListener('message', function(e) {
-    clients.filter(v => v!== port) // 此处为了贴近其他方案的实现，剔除自己
-		.forEach(p => p.postMessage(e.data))
+    clients.forEach(p => p.postMessage(e.data))
   });
 }
